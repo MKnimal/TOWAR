693189363f75842979a2d1901a0aa6b91b9908a8
MKnimal <bbaagggg2@gmail.com>
2022-10-25 08:44:15 -0600

    Animacion añadida

--- THA_TOWAR/Assets/Scripts/ENEMIES/EnemyIA.cs
+++ THA_TOWAR/Assets/Scripts/ENEMIES/EnemyIA.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScene2 : MonoBehaviour
+public class EnemyIA : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
