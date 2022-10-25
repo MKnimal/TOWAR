ffc1cd747600e7ece8eecc6f228acd53d94f8830
MKnimal <bbaagggg2@gmail.com>
2022-10-25 11:12:29 -0600

    Sistema de movimiento enemigo mejorado

--- THA_TOWAR/Assets/Scripts/ENEMIES/enemigopru.cs
+++ THA_TOWAR/Assets/Scripts/ENEMIES/enemigopru.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,42 @@ public class enemigopru : MonoBehaviour
 {
     private Rigidbody EnemigoRB;
     private GameObject Jugador;
-    private float speed = 3;
+    private Animator eAnimator;
+    private SpriteRenderer eRender;
+    private float speed = 15;
+    private float post;
+    private Vector3 normalRotation;
+    private Vector3 inverseRotation;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("Inicializando Enemigo");
+        eAnimator = GetComponent<Animator>();
+        eRender = GetComponent<SpriteRenderer>();
         EnemigoRB = GetComponent<Rigidbody>();
+        normalRotation = new Vector3(0, 0, 0);
+        inverseRotation = new Vector3(0, 180, 0);
         Jugador = GameObject.Find("Player");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        EnemigoRB.AddForce((Jugador.transform.position - transform.position).normalized * speed);
+        post = (Jugador.transform.position - transform.position).normalized.x;
+        EnemigoRB.AddForce((Vector3.right).normalized * speed * post);
+        if (post != 0f){
+                            if(post < 0){
+                                transform.eulerAngles = normalRotation;
+                                eAnimator.SetBool("Moving", true);
+                            }
+                            if(post > 0){
+                                transform.eulerAngles = inverseRotation;
+                                eAnimator.SetBool("Moving", true);
+                            }
+                        }else{
+                            eAnimator.SetBool("Moving", false);
+                        }
+
     }
 }
