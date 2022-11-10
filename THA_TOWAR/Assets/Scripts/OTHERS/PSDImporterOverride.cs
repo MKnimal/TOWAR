7e68e6137a21cc5d3fb033d9cee28a593743af94
MKnimal <bbaagggg2@gmail.com>
2022-11-10 13:57:26 -0700

    Ajustes de errores

--- THA_TOWAR/Assets/Scripts/OTHERS/PSDImporterOverride.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace UnityEditor.U2D.PSD
-{
-    [AssetImporters.ScriptedImporter(1, new string[0],new[] {"psd"} )]
-    internal class PSDImporterOverride : PSDImporter
-    {
-    }
-}
